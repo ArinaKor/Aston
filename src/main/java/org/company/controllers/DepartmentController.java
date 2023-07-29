7e670b3817abd65d654a4ea2466e4578d6007326
arina kornilova <90265699+ArinaKor@users.noreply.github.com>
2023-07-29 20:08:04 +0300

    add configs and controller

---
+++ src/main/java/org/company/controllers/DepartmentController.java
@@ -0,0 +1,19 @@
+package org.company.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DepartmentController {
+    
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> sayHello(){
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
+
+}
