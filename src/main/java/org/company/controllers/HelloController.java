00bb2ef4cbeed4bf1da933a9dd3a492fe609d41c
arina kornilova <90265699+ArinaKor@users.noreply.github.com>
2023-08-01 13:56:01 +0300

    test2

--- src/main/java/org/company/controllers/HelloController.java
+++ src/main/java/org/company/controllers/HelloController.java
@@ -1,8 +1,5 @@
 package org.company.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +10,7 @@ public class HelloController {
     
     @RequestMapping(method = RequestMethod.GET)
     public String sayHello(){
-        return "Hello world!!!";
+        return "Hello from Arina!!!";
     }
 
 }
